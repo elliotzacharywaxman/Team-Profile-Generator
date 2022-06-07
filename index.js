bb22501be5b8ad41a81dca825194863bf058f1c0
elliotzacharywaxman <elliot@climatedao.xyz>
2022-06-07 17:18:20 -0400

    added all files and basic logic

---
+++ index.js
@@ -0,0 +1,39 @@
+const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+// import {Employee, Engineer, Manager, Intern} from "../lib/classes.js"
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: 'What is you employee id?',
+      name: 'id',
+    },
+    {
+      type: 'input',
+      message: 'What is your email?',
+      name: 'email',
+    },
+    // add logic for if else
+    {
+      type: 'input',
+      message: 'What is your github?',
+      name: 'github',
+    },
+    {
+      type: 'input',
+      message: 'What school are you affiliated with?',
+      name: 'school',
+    },
+  ])
+  .then((response) => console.log(response));
+
+const Jason = new Employee()
